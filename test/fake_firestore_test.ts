7061d1686d1a8fcf141d48efb61752390b77831e
cedvdb <cedvandenbosch@gmail.com>
2023-05-31 18:41:57 +0200

    writing tests

---
+++ test/fake_firestore_test.ts
@@ -0,0 +1,45 @@
+import { CollectionReference, DocumentReference, Firestore } from 'firebase-admin/firestore';
+import assert from 'node:assert';
+import { describe, it, } from 'node:test';
+import { FakeFirestore } from '../src';
+
+
+
+describe('FakeFirestore', () => {
+  it('should implement firestore', () => {
+    const firestore: Firestore = new FakeFirestore({ accounts: {} });
+    const collection: CollectionReference = firestore.collection('accounts');
+    const document: DocumentReference = collection.doc('user-id-1');
+    assert(document != undefined);
+  });
+
+  it('should find collection');
+  it('should find collectionGroup');
+});
+
+describe('FakeCollection', () => {
+  it('should find document', () => { });
+  it('should add document');
+  describe('Query', () => {
+    it('should filter with >');
+    it('should filter with >=');
+    it('should filter with ==');
+    it('should filter with !=');
+    it('should filter with <=');
+    it('should filter with <');
+    it('should filter with array-contains');
+    it('should filter with array-contains-any');
+    it('should filter with in');
+    it('should filter with not-in');
+  });
+});
+
+
+describe('FakeDocument', () => {
+  it('should know if it exists');
+  it('should have data');
+  it('should create');
+  it('should set');
+  it('should update');
+  it('should delete');
+});
